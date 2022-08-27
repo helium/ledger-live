9169034580b545a63e2d7a4b5c986a1a9d486dcd
Luis Perrone <peronif5@gmail.com>
2022-08-26 23:48:34 -0500

    Add helium voting to ledger-live-desktop

--- apps/ledger-live-desktop/src/renderer/modals/index.js
+++ apps/ledger-live-desktop/src/renderer/modals/index.js
@@ -75,6 +75,9 @@ import MODAL_SOLANA_DELEGATION_DEACTIVATE from "../families/solana/DelegationDea
 import MODAL_SOLANA_DELEGATION_REACTIVATE from "../families/solana/DelegationReactivateFlowModal";
 import MODAL_SOLANA_DELEGATION_WITHDRAW from "../families/solana/DelegationWithdrawFlowModal";
 
+import MODAL_HELIUM_VOTE_INFO from "../families/helium/VoteFlowModal/Info";
+import MODAL_HELIUM_VOTE from "../families/helium/VoteFlowModal";
+
 // Lending
 import MODAL_LEND_MANAGE from "../screens/lend/modals/ManageLend";
 import MODAL_LEND_ENABLE_INFO from "../screens/lend/modals/EnableInfoModal";
@@ -144,6 +147,8 @@ const modals: { [_: string]: React$ComponentType<any> } = {
   MODAL_SOLANA_DELEGATION_DEACTIVATE,
   MODAL_SOLANA_DELEGATION_REACTIVATE,
   MODAL_SOLANA_DELEGATION_WITHDRAW,
+  MODAL_HELIUM_VOTE_INFO,
+  MODAL_HELIUM_VOTE,
   MODAL_FULL_NODE,
   MODAL_LOTTIE_DEBUGGER,
   MODAL_RECOVERY_SEED_WARNING,
